fb02785e2fe1c3c29c82aea11260d927bec709f3
Alvin P. Difuntorum <alvinpd09@gmail.com>
2022-06-19 14:03:47 +0200

    Updated external dependencies

--- tdjson_wrapper.cpp
+++ tdjson_wrapper.cpp
@@ -23,7 +23,9 @@ static WCHAR			wcstr[RCV_DATA_BUFFER_SIZE];
 
 /**
  * Returns an opaque identifier of a new TDLib instance.
- * The TDLib instance will not send updates until the first request is sent to it.
+ * The TDLib instance will not send updates until the first request
+ * is sent to it.
+ *
  * \return Opaque identifier of a new TDLib instance.
  */
 DLLFUNC int TDCreateClientId(void)
@@ -45,10 +47,16 @@ DLLFUNC void TDSend(int client_id, const LPCWSTR request)
 }
 
 /**
- * Receives incoming updates and request responses. Must not be called simultaneously from two different threads.
- * The returned pointer can be used until the next call to td_receive or td_execute, after which it will be deallocated by TDLib.
- * \param[in] timeout The maximum number of seconds allowed for this function to wait for new data.
- * \return JSON-serialized null-terminated incoming update or request response. May be NULL if the timeout expires.
+ * Receives incoming updates and request responses. Must not be called
+ * simultaneously from two different threads.
+ * The returned pointer can be used until the next call to td_receive or
+ * td_execute, after which it will be deallocated by TDLib.
+ *
+ * \param[in] timeout The maximum number of seconds allowed for this
+ *                    function to wait for new data.
+ *
+ * \return JSON-serialized null-terminated incoming update or request
+ *         response. May be NULL if the timeout expires.
  */
 DLLFUNC const LPCWSTR TDReceive(double timeout)
 {
@@ -76,8 +84,11 @@ DLLFUNC const LPCWSTR TDReceive(double timeout)
 
 /**
  * Synchronously executes a TDLib request.
- * A request can be executed synchronously, only if it is documented with "Can be called synchronously".
- * The returned pointer can be used until the next call to td_receive or td_execute, after which it will be deallocated by TDLib.
+ * A request can be executed synchronously, only if it is documented
+ * with "Can be called synchronously".
+ * The returned pointer can be used until the next call to td_receive
+ * or td_execute, after which it will be deallocated by TDLib.
+ *
  * \param[in] request JSON-serialized null-terminated request to TDLib.
  * \return JSON-serialized null-terminated request response.
  */
@@ -97,4 +108,3 @@ DLLFUNC const LPCWSTR TDExecute(const LPCWSTR request)
 
 	return wcstr;
 }
-
